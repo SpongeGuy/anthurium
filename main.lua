301ad747b602dcd52f233b79b86a06b44552ec07
Sponge Guy <realevan03@gmail.com>
2025-03-22 00:31:37 -0400

    `player:shoot()`, i'm gonna separate bullet from shoot into its own function to make a test enemy

--- main.lua
+++ main.lua
@@ -13,6 +13,21 @@ push:setupScreen(game_width, game_height, window_width, window_height, {fullscre
 local entities = {}
 local bullets = {}
 
+function vector_normalize(vector)
+	local length = math.sqrt(vector.x^2 + vector.y^2)
+	print(length)
+	print(vector.x, vector.y)
+	if length == 0 then
+		return {x = 0, y = 0}
+	else
+		return {x = vector.x / length, y = vector.y / length}
+	end
+end
+
+function vector_scalar_multiply(vector, multiplier)
+	return {x = vector.x * multiplier, y = vector.y * multiplier}
+end
+
 -- this is for triangle man
 function create_triangle_player(x, y)
 	local posX = x or 0
@@ -22,13 +37,18 @@ function create_triangle_player(x, y)
 		vel = {x = 0, y = 0},
 		max_speed = 100,
 		facing = 0,
-		attack_speed = 0.1
+
+		shoot_cooldown = 0,
+		shoot_attack_speed = 1,
+		bullet_velocity = 250,
 	}
-	function player:shoot()
-		-- this is not complete atm but will call attack_speed stat
+	function player:shoot(direction_vector)
+		if self.shoot_cooldown > 0 then return end
+
+		
 		local bullet = {
 			pos = {x = player.pos.x, y = player.pos.y},
-			vel = {x = dx, y = dy},
+			vel = direction_vector,
 			speed = speed,
 		}
 		function bullet:update(dt)
@@ -36,8 +56,11 @@ function create_triangle_player(x, y)
 			bullet.pos.y = bullet.pos.y + bullet.vel.y * dt
 		end
 		function bullet:draw()
-			love.graphics.circle('fill', bullet.pos.x, bullet.pos.y, 5)
+			love.graphics.circle('fill', bullet.pos.x, bullet.pos.y, 2.5)
 		end
+
+		self.shoot_cooldown = self.shoot_attack_speed / 5
+		table.insert(bullets, bullet)
 	end
 	function player:update(dt)
 		-- use this in love:update, probably add this to some overarching table like dudes {}
@@ -49,7 +72,7 @@ function create_triangle_player(x, y)
 
 		-- normalize movement here
 		local vel_len = math.sqrt(player.vel.x^2 + player.vel.y^2)
-		print(vel_len)
+		
 		if vel_len > 0 then
 			player.vel.x = player.vel.x / vel_len
 			player.vel.y = player.vel.y / vel_len
@@ -59,6 +82,15 @@ function create_triangle_player(x, y)
 		player.pos.x = player.pos.x + player.vel.x * player.max_speed * dt
 		player.pos.y = player.pos.y + player.vel.y * player.max_speed * dt
 		player.facing = math.atan2(m_y - player.pos.y, m_x - player.pos.x)
+
+		-- combat cooldowns
+		player.shoot_cooldown = math.max(0, player.shoot_cooldown - dt)
+
+		
+		-- shoot mechanic
+		if love.mouse.isDown(1) then
+			player:shoot(vector_scalar_multiply(vector_normalize({x = m_x - player.pos.x, y = m_y - player.pos.y}), player.bullet_velocity))
+		end
 	end
 	function player:draw()
 
