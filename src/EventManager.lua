c94cae52347b6d393c02087f500230f68696966e
Sponge <realevan03@gmail.com>
2025-04-08 22:46:46 -0400

    draw bouncy exclamation mark function and refined drone movement

--- src/EventManager.lua
+++ src/EventManager.lua
@@ -4,3 +4,17 @@ EventManager = {
 	listeners = {}
 }
 
+function EventManager:subscribe(event_type, callback)
+	if not self.listeners[event_type] then
+		self.listeners[event_type] = {}
+	end
+	table.insert(self.listeners[event_type], callback)
+end
+
+function EventManager:broadcast(event_type, data)
+	if self.listeners[event_type] then
+		for _, callback in ipairs(self.listeners[event_type]) do
+			callback(data)
+		end
+	end
+end
--- src/entities/drone.lua
+++ src/entities/drone.lua
@@ -34,6 +34,16 @@ function vector_to_degrees(vector)
 	return angle_degrees
 end
 
+function draw_bouncy_exclamation_mark(x, y, timer)
+	local A = 25
+	local l = 0.5
+	local e = 2.71828
+	local h = 0.9
+	local B = 1.5
+	-- bouncy exclamation mark equation
+	love.graphics.print("!", math.floor(x) - 1, math.floor(y - (A*e^(-l*(timer^h)) * math.abs(math.sin(B*timer))) - 7))
+end
+
 local drone_states = {}
 
 drone_states.Wandering = {
@@ -41,6 +51,7 @@ drone_states.Wandering = {
 		-- create a node between 100 and 20 units away from the x, y
 		local dist = 20 + math.random() * 40
 		self.move_node = get_random_coordinate_away(self.pos.x, self.pos.y, dist)
+
 		-- set velocity towards move_node
 		self.vel = vector_normalize({x = self.move_node.x - self.pos.x, y = self.move_node.y - self.pos.y})
 		self.vel = {x = self.vel.x * 25, y = self.vel.y * 25}
@@ -52,6 +63,7 @@ drone_states.Wandering = {
 		self.pos.x = self.pos.x + self.vel.x * dt
 		self.pos.y = self.pos.y + self.vel.y * dt
 
+		-- visual bullshit
 		if self.size < 4 then
 			self.size = 6
 		end
@@ -101,13 +113,29 @@ drone_states.Wandering = {
 
 drone_states.Waiting = {
 	enter = function(self)
-		self.vel = {x = 0, y = 0}
+		
 		self.timer = 0
 		self.random_wait_value = math.random(2, 5)
 		self.size = 5
 	end,
 
 	update = function(self, dt)
+		-- slow down velocity smoothly
+		if math.abs(self.vel.x) > 0 or math.abs(self.vel.y) > 0 then
+			self.vel.x = self.vel.x / 1.02
+			self.vel.y = self.vel.y / 1.02
+			if math.abs(self.vel.x) < 1 then
+				self.vel.x = 0
+			end
+			if math.abs(self.vel.y) < 1 then
+				self.vel.y = 0
+			end
+		end
+
+		-- update position based on velocity
+		self.pos.x = self.pos.x + self.vel.x * dt
+		self.pos.y = self.pos.y + self.vel.y * dt
+
 		self.timer = self.timer + dt
 		self.size = (math.sin(self.timer)) + 4
 		if self.timer > self.random_wait_value then
@@ -131,15 +159,17 @@ drone_states.Waiting = {
 }
 
 drone_states.Pursuing = {
-	-- timer is for initial
+	-- timer is for exclamation mark graphic
 	timer = 0,
 	mod_timer = 0,
 	enter = function(self)
 		timer = 0
 		mod_timer = 0
+		self.size = 5
 	end,
 
 	update = function(self, dt)
+		-- move towards target steadily
 		local distX = self.target.pos.x - self.pos.x
 		local distY = self.target.pos.y - self.pos.y
 		self.vel = vector_normalize({x = distX, y = distY})
@@ -169,29 +199,23 @@ drone_states.Pursuing = {
 
 	draw = function(self)
 		if timer then
+			love.graphics.setColor(0, 0, 0)
+			draw_bouncy_exclamation_mark(self.pos.x+1, self.pos.y+1, mod_timer)
+			love.graphics.setColor(1, 0.1, 0.3)
 			draw_bouncy_exclamation_mark(self.pos.x, self.pos.y, mod_timer)
+			love.graphics.setColor(1, 1, 1)
 		end
 	end
 }
 
-function draw_bouncy_exclamation_mark(x, y, timer)
-	love.graphics.setColor(1, 0.1, 0.3)
-	local A = 25
-	local l = 0.5
-	local e = 2.71828
-	local h = 0.9
-	local B = 1.5
-	-- bouncy exclamation mark equation
-	love.graphics.print("!", math.floor(x) - 1, math.floor(y - (A*e^(-l*(timer^h)) * math.abs(math.sin(B*timer))) - 7))
-	love.graphics.setColor(1, 1, 1)
-end
+
 
 drone_states.Hungry = {
 
 }
 
 drone_states.Attacking = {
-
+	-- figure out how to bounce
 }
 
 function create_drone(posX, posY)
@@ -201,8 +225,8 @@ function create_drone(posX, posY)
 		vel = {x = 0, y = 0},
 		hunger = 0,
 		size = 5,
-		aggro_range = 75,
-		forget_range = 125, 
+		aggro_range = 50,
+		forget_range = 50, 
 	}
 
 	drone.state_machine.entity = drone
@@ -215,12 +239,15 @@ function create_drone(posX, posY)
 
 	function drone:update(dt)
 		self.state_machine:update(dt)
+		self.hunger = self.hunger + dt
 	end
 
 	function drone:draw()
 		
 		love.graphics.circle('fill', self.pos.x, self.pos.y, self.size)
 		self.state_machine:draw()
+		love.graphics.setColor(0, 0, 0)
+		love.graphics.print(math.floor(self.hunger), math.floor(self.pos.x), math.floor(self.pos.y))
 	end
 
 	return drone
